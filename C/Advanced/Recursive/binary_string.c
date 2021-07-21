7be664d33afc612174d48f6185dd8c9ff26ff49d
MattB17 <matt.buckley1755@gmail.com>
2021-07-21 11:07:01 -0400

    Finishing Advanced C Programming

--- C/Advanced/Recursive/binary_string.c
+++ C/Advanced/Recursive/binary_string.c
@@ -5,3 +5,27 @@
 // generate all strings of length n + 1 by adding a 0
 // to every string of length n and by adding a 1 to every
 // string of length n
+#include <stdio.h>
+
+#define N 4
+
+void binstrs(char bits[], int index) {
+  if (index == N) {
+    printf("%s\n", bits);
+    return;
+  }
+
+  bits[index] = '0';
+  binstrs(bits, index + 1);
+
+  bits[index] = '1';
+  binstrs(bits, index + 1)
+}
+
+int main() {
+  char binary_num[N + 1];
+  binary_num[N] = '\0';
+
+  binstrs(binary_num, 0);
+  return 0;
+}
--- C/Notes/complexity.md
+++ C/Notes/complexity.md
@@ -7,3 +7,16 @@ Big-O notation
 
 `f(n) = O(g(n))`
 * means there are positive constants `c` and `k` such that `0 <= f(n) <= cg(n)` for all `n >= k`
+
+### Recursion
+Recursive definition of a string:
+```
+Definition:
+  string := '\0'
+         := char, string
+
+Examples:
+  "\0"            -- '\0'
+  "r"             -- 'r', '\0'
+  "roses are red" -- 'r', 'oses are red\0'
+```
