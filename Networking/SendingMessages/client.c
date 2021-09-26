93255f101f368009dcb464f7443884a7a6295eb7
MattB17 <matt.buckley1755@gmail.com>
2021-09-26 01:57:45 -0400

    Adding sending messages example

---
+++ Networking/SendingMessages/client.c
@@ -0,0 +1,64 @@
+/* The client side takes the name of the remote machine as an argument.
+It calls the Linux utility to translate this name into the remote host's IP
+address. The next step is to construct the address data structure (sin)
+expected by the socket interface. The final step in setting up the connection
+is to call socket and connect. Once the operation returns, the connection is
+established and the client program enters its main loop, which reads text from
+standard input and sends it over the socket
+*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define SERVER_PORT 5432
+#define MAX_LINE 256
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  struct hostent *hp;
+  struct sockaddr_in sin;
+  char *host;
+  char buf[MAX_LINE];
+  int s;
+  int len;
+
+  if (argc==2) {
+    host = argv[1];
+  } else {
+    fprintf(stderr, "usage: simplex-talk host\n");
+    exit(1);
+  }
+
+  // translate host name into peer's IP address
+  hp = gethostbyname(host);
+  if (!hp) {
+    fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+    exit(1);
+  }
+
+  // build address data structure
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+  sin.sin_port = htons(SERVER_PORT);
+
+  // active open
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+  if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    perror("simplex-talk: connect");
+    close(s);
+    exit(1);
+  }
+
+  // main loop: get and send lines of text
+  while (fgets(buf, sizeof(buf), stdin)) {
+    buf[MAX_LINE - 1] = '\0';
+    len = strlen(buf) + 1;
+    send(s, buf, len, 0);
+  }
+}
